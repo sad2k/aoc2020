0ea17fab99e1d54a434ccdabbada1ae62b82f0c1
Sergey Dubakov <serge.dubakov@gmail.com>
2020-12-02 19:51:53 +0000

    renamed

--- src/Day1Puzzle2.hs
+++ src/Day1Puzzle2.hs
@@ -2,11 +2,13 @@
 import System.IO
 
 findSolution :: [Int] -> Int
-findSolution l = head [x*y | x <- l, y <- l, x<y, x+y == 2020]
+findSolution l = head [x*y*z | x <- l, y <- l, z <- l, x<y, y<z, x+y+z == 2020]
 
 run h = do
     contents <- hGetContents h
     print $ findSolution $ map read $ lines contents
 
-main = withFile "input/Puzzle1.txt" ReadMode run
+main = withFile "input/Day1.txt" ReadMode run
+
+
 
