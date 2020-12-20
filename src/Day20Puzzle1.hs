4d0b9f2b867c74a9be5b9eb27e028571c2fda149
Sergey Dubakov <serge.dubakov@gmail.com>
2020-12-20 19:11:55 +0000

    day 20 puzzle 1 - tidy

--- src/Day20Puzzle1.hs
+++ src/Day20Puzzle1.hs
@@ -48,8 +48,8 @@ squareSize n = round $ sqrt (fromIntegral n)
 
 drawTile t = mapM_ print (snd t)
 
-sumOfCorners :: Int -> [Tile] -> Integer
-sumOfCorners n xs = product $ map (\x -> toInteger $ fst $ xs !! (toIdx x)) coords
+corners :: Int -> [Tile] -> [Integer]
+corners n xs = map (\x -> toInteger $ fst $ xs !! (toIdx x)) coords
     where toIdx (x,y) = y * n + x
           coords = [(0,0),(0,n-1),(n-1,0),(n-1,n-1)]
 
@@ -57,41 +57,11 @@ run h = do
     contents <- hGetContents h
     let ls = lines contents
     let tiles = map parseTile (splitOn [""] ls)
---     mapM_ print tiles
     let sqsz = squareSize (length tiles)
     print $ sqsz
---     print sqsz
     let res = placeTiles sqsz [] tiles
-    print $ map (\x -> sumOfCorners sqsz x) res
-
---     print $ allowedTiles sqsz [tiles !! 1] (tiles)
---     mapM_ print $ (transpose . snd) (tiles !! 1)
---     print "--"
---     mapM_ print $ (snd . head) tiles
---     print "--"
---     mapM_ print $ (snd . last) tiles
-   -- testing
---     let tile1 = flipVertically $ tiles !! 1
---     let except1 = filter (exceptTile tile1) tiles
---     let tile2 = head $ allowedTiles sqsz [tile1] except1
---     let except2 = filter (exceptTile tile2) except1
---     let tile3 = head $ allowedTiles sqsz [tile1, tile2] except2
---     let except3 = filter (exceptTile tile3) except2
---     let tile4 = head $ allowedTiles sqsz [tile1, tile2, tile3] except3
---     let except4 = filter (exceptTile tile4) except3
---     let tile5 = head $ allowedTiles sqsz [tile1, tile2, tile3, tile4] except4
---     let except5 = filter (exceptTile tile5) except4
---     let tile6 = head $ allowedTiles sqsz [tile1, tile2, tile3, tile4, tile5] except5
---     let except6 = filter (exceptTile tile6) except5
---     let tile7 = head $ allowedTiles sqsz [tile1, tile2, tile3, tile4, tile5, tile6] except6
---     let except7 = filter (exceptTile tile7) except6
---     let tile8 = head $ allowedTiles sqsz [tile1, tile2, tile3, tile4, tile5, tile6, tile7] except7
---     let except8 = filter (exceptTile tile8) except7
---     print $ allowedTiles sqsz [tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8] except8
---     let testTile = (0, ["123","456","789"])
---     drawTile $ (rotateLeft . flipVertically) testTile
---     print "---"
---     drawTile $ (rotateLeft . flipHorizontally) testTile
-
+    let crn = map (\x -> corners sqsz x) res
+    print crn
+    print $ map product corners
 
 main = withFile "input/Day20.txt" ReadMode run
